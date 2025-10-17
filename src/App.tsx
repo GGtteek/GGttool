6b8b5d7e3d35d549d418c7c0c3fff81de9e8fe0f
GGtteek <d4nt3c4rd0s@gmail.com>
2025-10-17 12:53:25 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import { Layout } from 'antd';
+
+const { Header, Footer, Content } = Layout;
+
+function App() {
+
+  return (
+    <Layout >
+      <Header >Header</Header>
+      <Content >Content</Content>
+      <Footer >Footer</Footer>
+    </Layout>
+  );
+}
+
+export default App;
