89a5d035ac65780d4130acab6cc5f0ade7830f0c
GGtteek <d4nt3c4rd0s@gmail.com>
2025-10-18 11:07:00 +0800

    clean copy

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <Layout >
-      <Header >Header Testing 2</Header>
+      <Header >Header</Header>
       <Content >Content</Content>
       <Footer >Footer</Footer>
     </Layout>
